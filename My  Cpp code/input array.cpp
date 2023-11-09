9d86461cab5870e0f77a35594563cba220b3a5b2
shishir mondal <128738233+shishirmondal13@users.noreply.github.com>
2023-11-09 23:25:00 +0600

    Create input array.cpp

---
+++ My  Cpp code/input array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+   int i;
+   int num[6];
+   cout<<"Enter number:";
+   for(i=0;i<5;i++)
+   {
+      cin>>num[i];
+   }
+   cout<<"number is:";
+   for(i=0;i<5;i++)
+   {
+      cout<<num[i]<<"  ";
+   }
+
+}

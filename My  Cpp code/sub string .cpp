2ab8bbcbde82ecce79d8c75b87f1d20555c438d0
shishir mondal <128738233+shishirmondal13@users.noreply.github.com>
2024-01-18 02:43:06 +0600

    Add files via upload

---
+++ My  Cpp code/sub string .cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+string str = "javatpoint";
+string substring = "";
+substring = str.substr(0,10);
+cout<<"Substring is : "<< substring;
+return 0;
+}

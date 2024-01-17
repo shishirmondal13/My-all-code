89f73ffea5040ef832074ef87313716be8a212f1
shishir mondal <128738233+shishirmondal13@users.noreply.github.com>
2024-01-18 02:45:29 +0600

    Add files via upload

---
+++ My  Cpp code/string copy.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+#include<cstring>
+using namespace std;
+
+using namespace std;
+
+int main()
+{
+    char str[]="shishir mondal", target[20];
+
+    strcpy(target,str);
+
+    cout << "Source string = " << str<<endl;
+    cout << "Target string = " << target<<endl;
+}
